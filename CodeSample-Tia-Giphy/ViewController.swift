a90691ed6f4a9f3f62b84976847c97edb8ea03e8
Sadie Contini <scontini@yahoo.com>
2018-02-26 09:22:01 -0800

    Initial Commit

---
+++ CodeSample-Tia-Giphy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeSample-Tia-Giphy
+//
+//  Created by sadie on 2/26/18.
+//  Copyright © 2018 Sadie Contini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
